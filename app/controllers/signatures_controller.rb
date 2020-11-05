746652fa4d1a895ab27a4993d54554cb1c42a48b
machado-jose <machado.josegustavo@gmail.com>
2020-11-05 13:36:47 +0000

    Download Document

--- app/controllers/signatures_controller.rb
+++ app/controllers/signatures_controller.rb
@@ -1,4 +1,19 @@
 class SignaturesController < ApplicationController
   def index
+    if Signatory.exists?(params[:signatory_id])
+      @signatory = Signatory.find(params[:signatory_id])
+    else
+      # Error Page
+      puts ">>>>>>>>>>>>>>>> Erro Index Sign"
+      render :index
+    end 
+  end
+
+  def download_file
+    download_file = Solicitation.find(params[:solicitation_id])
+    send_file Rails.root.join('storage', download_file.document.path),
+    :filename => download_file.document_file_name,
+    :type => download_file.document_content_type,
+    :disposition => 'attachment'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :signatures, only: [:index]
   resources :solicitations, except: [:edit, :update]
-
+  get 'signature/:signatory_id', to: 'signatures#index', as: 'signature'
+  get 'signature/download/:solicitation_id', to: 'signatures#download_file', as: 'document_download_signature'
   root to: 'solicitations#index'
 end
