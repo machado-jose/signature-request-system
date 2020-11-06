aab2b8cd5bdd2dbd9722ea7bc922108ca0bc3a09
machado-jose <machado.josegustavo@gmail.com>
2020-11-06 00:01:45 +0000

    Error message in the case download fail

--- app/controllers/signatures_controller.rb
+++ app/controllers/signatures_controller.rb
@@ -2,9 +2,11 @@ class SignaturesController < ApplicationController
 
   def index
     @signatory = Signatory.find_by(id: params[:signatory_id])
+    
     unless @signatory
       render :error
     end
+
     @signatory
   end
 
@@ -12,14 +14,14 @@ class SignaturesController < ApplicationController
     begin
       download_file = Solicitation.find(params[:solicitation_id])
 
-      send_file Rails.root.join('torage', download_file.document.path),
+      send_file Rails.root.join('storage', download_file.document.path),
       :filename => download_file.document_file_name,
       :type => download_file.document_content_type,
       :disposition => 'attachment'
 
     rescue ActionController::MissingFile, ActiveRecord::RecordNotFound => exception
       puts "[X] Error in signatures#download_file: #{exception}"
-      render :index
+      redirect_to signature_path(params[:signatory_id]), notice: 'Error while downloading. Send an email to the Call Center for any clarification'
     end
   end
 
