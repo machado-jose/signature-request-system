e8c057116c8525d87521a7c4a9b5ee1e216f1424
machado-jose <machado.josegustavo@gmail.com>
2020-11-08 20:01:50 +0000

    Get solicitation status

--- app/helpers/solicitations_helper.rb
+++ app/helpers/solicitations_helper.rb
@@ -1,2 +1,12 @@
 module SolicitationsHelper
+  def get_solicitation_status(solicitation)
+    signatures_list = Signature.where(:solicitation_id => solicitation.id).to_a
+    if signatures_list.all?{|signature| signature.is_signed}
+      'Approved'
+    elsif signatures_list.any?{|signature| signature.denied}
+      'Denied'
+    else 
+      'Pending'
+    end
+  end
 end
